1132567e6eb0140a2c445aa2a8aee089ecf9555d
Manan <manangoel012@gmail.com>
2020-05-10 15:55:30 +0530

    fix

---
+++ src/AnnotationTool/App.js
@@ -0,0 +1,191 @@
+import React from 'react';
+import { Stage, Layer } from 'react-konva';
+import shortid from 'shortid';
+import Rectangle from './Rectangle/Rectangle';
+import RectTransformer from './Rectangle/RectTransformer';
+import AnnotationImage from './AnnotationImage/AnnotationImage';
+import './App.css';
+import Sidebar from './Sidebar/Sidebar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+  state = {
+    rectangles: [],
+    rectCount: 0,
+    selectedShapeName: '',
+    mouseDown: false,
+    mouseDraw: false,
+    newRectX: 0,
+    newRectY: 0,
+    i: 0,
+  };
+
+  componentDidMount() {
+    this.img.moveToBottom();
+  }
+
+  handleStageMouseDown = (event) => {
+    const { rectangles } = this.state;
+    // clicked on stage - clear selection or ready to generate new rectangle
+    if (event.target.className === 'Image') {
+      const stage = event.target.getStage();
+      const mousePos = stage.getPointerPosition();
+      this.setState({
+        mouseDown: true,
+        newRectX: mousePos.x,
+        newRectY: mousePos.y,
+        selectedShapeName: '',
+      });
+      return;
+    }
+    // clicked on transformer - do nothing
+    const clickedOnTransformer = event.target.getParent().className === 'Transformer';
+    if (clickedOnTransformer) {
+      return;
+    }
+
+    // find clicked rect by its name
+    const name = event.target.name();
+    console.log(name);
+    const rect = rectangles.find(r => r.name === name);
+    if (rect) {
+      this.setState({
+        selectedShapeName: name,
+        rectangles,
+      });
+    } else {
+      this.setState({
+        selectedShapeName: '',
+      });
+    }
+  };
+
+  // When a properties of rectangle are changed
+  handleRectChange = (index, newProps) => {
+    const { rectangles } = this.state;
+    rectangles[index] = {
+      ...rectangles[index],
+      ...newProps,
+    };
+
+    this.setState({ rectangles });
+  };
+
+  // when a new rectangle is drawn
+  handleNewRectChange = (event) => {
+    const {
+      rectangles, rectCount, newRectX, newRectY,
+    } = this.state;
+    const stage = event.target.getStage();
+    const mousePos = stage.getPointerPosition();    // get mouse position
+    if (!rectangles[rectCount]) {
+      rectangles.push({
+        x: newRectX,
+        y: newRectY,
+        width: mousePos.x - newRectX,
+        height: mousePos - newRectY,
+        name: '',
+        stroke: '#00A3AA',
+        key: shortid.generate(),
+      });
+      return this.setState({ rectangles, mouseDraw: true });
+    }
+    rectangles[rectCount].width = mousePos.x - newRectX;
+    rectangles[rectCount].height = mousePos.y - newRectY;
+    return this.setState({ rectangles });
+  };
+
+  handleStageMouseUp = () => {
+    var this1 = this;
+
+    const { rectCount, mouseDraw } = this.state;
+    const { rectangles, i } = this.state;
+    if (mouseDraw) {
+      this.setState({ rectCount: rectCount + 1, mouseDraw: false });
+      var annotations = document.getElementById('annotate');
+      var e1 = annotations.appendChild(document.createElement('input'));    // creating input field
+      e1.className = 'fieldClass';
+      e1.onchange = updateName;
+
+      function updateName() {       //Local function to update name
+        var value = this.value;
+        console.log(value);
+        this1.setState(Object.assign(this1.state.rectangles[i], { name: value }));
+      }
+      console.log(this1.state.rectangles);
+      console.log(rectangles[i].name);
+      this.setState({
+        i: i + 1
+
+        //this.state.rectangles[this.state.i].name = document.querySelector('input[className="fieldCLass"]');
+        //this1.state.i = this1.state.i + 1;
+      });
+    }
+
+    //console.log(this1.state.rectangles);
+    this.setState({ mouseDown: false });
+  };
+
+  render() {
+    const {
+      state: { rectangles, selectedShapeName, mouseDown },
+      handleStageMouseDown,
+      handleNewRectChange,
+      handleRectChange,
+      handleStageMouseUp,
+    } = this;
+    return (
+      <div style={{width:'100%'}}>
+        <div className="row">
+          <div className="sm spa">
+            <Sidebar />
+          </div>
+          <div className="col" id="app">
+            <Stage
+              ref={(node) => {
+                this.stage = node;
+              }}
+              container="app"
+              width={window.innerWidth*0.8}
+              height={window.innerHeight*0.95}
+              onMouseDown={handleStageMouseDown}
+              onTouchStart={handleStageMouseDown}
+              onMouseMove={mouseDown && handleNewRectChange}
+              onTouchMove={mouseDown && handleNewRectChange}
+              onMouseUp={mouseDown && handleStageMouseUp}
+              onTouchEnd={mouseDown && handleStageMouseUp}
+            >
+              <Layer>
+                {rectangles.map((rect, i) => (
+                  <Rectangle id="annotate"
+                    sclassName="rect"
+                    key={rect.key}
+                    {...rect}
+                    onTransform={(newProps) => {
+                      handleRectChange(i, newProps);
+                    }}
+
+                  />
+                ))}
+                <RectTransformer selectedShapeName={selectedShapeName} />
+              </Layer>
+              <Layer
+                ref={(node) => {
+                  this.img = node;
+                }}
+              >
+                <AnnotationImage />
+              </Layer>
+            </Stage>
+          </div>
+          <div className="col" id="annotate">
+        <h3 style={{textAlign:'right'}}>Annotations</h3>
+      </div>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/AnnotationTool/Sidebar/Sidebar.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+import "./Sidebar.css"
+import axios from 'axios';
+import '../../../node_modules/@fortawesome/fontawesome-free/css/all.css'
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      dta: ''
+    }
+  }
+
+
+
+  uploadImage = () => {
+    var upload = document.querySelector('canvas');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+    reader.onloadend = function () {
+      upload.style.backgroundImage = `url(${reader.result})`;
+    }
+    if (file) {
+      reader.readAsDataURL(file);
+
+      // console.log(file);
+      var data = JSON.stringify({ "name": file.name, "Image type": file.type, "Image Size(KB)": (file.size) / 1024, "Last Modified Date": file.lastModifiedDate });
+      this.setState({ dta: data });
+      // console.log(data);
+      // axios
+      //     .post('https://labell.herokuapp.com/api/generate', data)
+      //     .then(() => console.log('Data Send'))
+      //     .catch(err => {
+      //       console.error(err);
+      //     });
+    }
+  }
+
+  DataSend() {
+    axios.post('https://labell.herokuapp.com/api/generate', this.state.dta)
+      .then(console.log('data send!'))
+      .then(
+        axios({
+          url: 'https://labell.herokuapp.com/api/getfile',
+          method: 'GET',
+          responseType: 'blob',
+        }).then((response) => {
+          console.log(response.data);
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement('a');
+          link.href = url;
+          link.setAttribute('download', 'file.json'); //or any other extension
+          document.body.appendChild(link);
+          link.click();
+
+        }).catch(err => {
+          console.log('error1', err);
+        }))
+      .catch(err => { console.log('error2', err) })
+  }
+
+  // getFile() {
+  //   axios({
+  //     url: 'https://labell.herokuapp.com/api/getfile',
+  //     method: 'GET',
+  //     responseType: 'blob',
+  //   })
+  //   .then((response) => {
+  //     // console.log(response.data);
+  //     const url = window.URL.createObjectURL(new Blob([response.data]));
+  //     const link = document.createElement('a');
+  //     link.href = url;
+  //     link.setAttribute('download', 'file.json'); //or any other extension
+  //     document.body.appendChild(link);
+  //     link.click();
+
+  //   });
+  // }
+
+  render() {
+    return (
+      <div className="wholeSidebar">
+        <div>
+          <label htmlFor="files" className="submitButton1" title="Select File to Upload"><i className="fas fa-file-upload fa-3x"></i></label>
+          <input id="files" accept="image/*" hidden="hidden" type="file" onChange={this.uploadImage} />
+        </div>
+        <div>
+           <button className="submitButton" title="Draw Rectangle">
+           <i className="fas fa-vector-square fa-3x"></i>
+          </button>
+        </div>
+        <div>
+          <button className="submitButton" title="Draw Circle">
+          <i className="far fa-circle fa-3x"></i>
+          </button>
+        </div>
+        <div>
+          <button className="submitButton" title="Draw Line">
+          <i className="fas fa-pen fa-2x"></i>
+          </button>
+        </div>
+        <div>
+          <button className="submitButton" title="Draw Polygon">
+          <i className="fas fa-draw-polygon fa-3x"></i>
+
+
+          </button>
+        </div>
+        {/* <div>
+          <button className="submitButton" title="Draw Point">
+            <p>Point</p>
+          </button>
+        </div> */}
+        <div>
+          <button className="submitButton" title="Save JSON">
+          <i className="fas fa-download fa-3x"></i>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Sidebar;
