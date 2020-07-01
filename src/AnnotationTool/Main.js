4864f35194479068949464e706752d5f7dfbfa02
Niket Shah <4917933-Niket97@users.noreply.gitlab.com>
2020-07-01 14:29:18 +0530

    Sign Up and Login changes

--- src/AnnotationTool/Main.js
+++ src/AnnotationTool/Main.js
@@ -46,13 +46,13 @@ class App extends React.Component {
     if (this.state.selectedImage && this.state.selectedImage.idx === idx) {
       this.setState({
         images: this.state.images.map((image, i) =>
-          i === idx ? {...image, ...updatedImages} : image
+          i === idx ? { ...image, ...updatedImages } : image
         ),
         selectedImage: {
           idx: idx,
           ...this.state.images[idx],
-          ...updatedImages
-        }
+          ...updatedImages,
+        },
       });
       return;
     }
