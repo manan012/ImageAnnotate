c191be3ffcddc5fc24603b8f71050925e260b416
Rajeev Kumar Yadav <rajeev011yadav@gmail.com>
2020-07-15 10:14:22 +0530

    added spinner for duration token verification is going on

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { connect } from "react-redux";
 import Datasetdisplay from "./Dashboards/ProjectDashBoard/Datasetdisplay";
 import Header from "./Header.js/Header";
 import DisplayErrors from "./Errors/DisplayErrors";
+import { Spinner } from "reactstrap";
 
 
 // import Axios from "axios";
@@ -33,70 +34,78 @@ class App extends Component {
     return (
         <div className="root">
           <DisplayErrors />
-          {this.props.loggedIn ? (
-            <Router>
-              <Header />
-              <Switch>
-                <Route
-                  exact
-                  path="/label"
-                  render={(props) => (
-                    <Main {...props} handleUser={this.signOutUser} />
-                  )}
-                ></Route>
-                <Route exact path="/projects" component={ProjectDashBoard} />
-                <Route exact path="/members" component={ProjectDashBoard} />
-                <Route exact path="/datasets" component={ProjectDashBoard} />
-                <Route exact path="/overview/:projectId" component={OverviewDashBoard} />
-                <Route exact path="/dataset/:datasetId" component={Datasetdisplay} />
-                <Route
-                  exact
-                  path="/"
-                  component={() => <Redirect to="/projects" />}
-                />
-                <Route
-                  exact
-                  path="/signin"
-                  component={() => <Redirect to="/projects" />}
-                />
-              </Switch>
-            </Router>
-          ) : (
-            <Router>
-              <Switch>
-                <Route exact path="/" component={Start}></Route>
-                <Route
-                  exact
-                  path="/signin"
-                  render={(props) => (
-                    <Signin {...props} handleUser={this.signInUser} />
-                  )}
-                ></Route>
-                <Route
-                  exact
-                  path="/label"
-                  component={() => <Redirect to="/signin" />}
-                />
-                <Route
-                  exact
-                  path="/projects"
-                  component={() => <Redirect to="/signin" />}
-                />
-                <Route
-                  exact
-                  path="/overview"
-                  component={() => <Redirect to="/signin" />}
-                />
-                {/* <Route path="*" component={() => <Redirect to="/" />} /> */}
-              </Switch>
-            </Router>
-          )}
+          {
+            this.props.status==="VERIFYING_TOKEN" ?
+            <div className="vh-100 d-flex align-items-center justify-content-center">
+              <Spinner color="primary" />
+            </div>
+            :
+            this.props.loggedIn ? (
+              <Router>
+                <Header />
+                <Switch>
+                  <Route
+                    exact
+                    path="/label"
+                    render={(props) => (
+                      <Main {...props} handleUser={this.signOutUser} />
+                    )}
+                  ></Route>
+                  <Route exact path="/projects" component={ProjectDashBoard} />
+                  <Route exact path="/members" component={ProjectDashBoard} />
+                  <Route exact path="/datasets" component={ProjectDashBoard} />
+                  <Route exact path="/overview/:projectId" component={OverviewDashBoard} />
+                  <Route exact path="/dataset/:datasetId" component={Datasetdisplay} />
+                  <Route
+                    exact
+                    path="/"
+                    component={() => <Redirect to="/projects" />}
+                  />
+                  <Route
+                    exact
+                    path="/signin"
+                    component={() => <Redirect to="/projects" />}
+                  />
+                </Switch>
+              </Router>
+            ) : (
+              <Router>
+                <Switch>
+                  <Route exact path="/" component={Start}></Route>
+                  <Route
+                    exact
+                    path="/signin"
+                    render={(props) => (
+                      <Signin {...props} handleUser={this.signInUser} />
+                    )}
+                  ></Route>
+                  <Route
+                    exact
+                    path="/label"
+                    component={() => <Redirect to="/signin" />}
+                  />
+                  <Route
+                    exact
+                    path="/projects"
+                    component={() => <Redirect to="/signin" />}
+                  />
+                  <Route
+                    exact
+                    path="/overview"
+                    component={() => <Redirect to="/signin" />}
+                  />
+                  {/* <Route path="*" component={() => <Redirect to="/" />} /> */}
+                </Switch>
+              </Router>
+            )
+          }
         </div>
     );
   }
 }
 
 const matchStateToProp = (store) => ({
+  status: store.user.status,
   loggedIn: store.user.loggedIn
 })
 
